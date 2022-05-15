0b477f8beeea879a61d5b6082d75bb348d7e82b3
azimut <azimut.github@protonmail.com>
2022-05-15 04:52:50 -0300

    minor: refactor, var initialization

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 #define DT_DPMS 5.0f
 #define DT_SUSPENSION 5.0f
-#define SLEEP_TIME 120
+#define WHILE_SLEEP_TIME 120
 #define SLEEP_FILE "sleeptime"
 #define AWAKE_FILE "awaketime"
 
--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 
 #include <X11/Xlib.h>
 #include <X11/extensions/dpms.h>
-#include <assert.h>
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -17,6 +16,13 @@
 #include <unistd.h>
 #include <upower.h>
 
+bool looping = true;
+
+void stop(__attribute__((unused)) int sig) {
+  printf("Stopping loop...\n");
+  looping = false;
+}
+
 float dt_hours(time_t a, time_t b) { return (a - b) / 60.0f / 60.0f; }
 
 bool dpms_wake(CARD16 prev_pw, CARD16 pw) {
@@ -28,39 +34,25 @@ bool dpms_sleep(CARD16 prev_pw, CARD16 pw) {
 }
 
 bool suspension_wake(time_t now, time_t before) {
-  assert(now > before);
-  return ((now - before) > (SLEEP_TIME * 3)) && before != 0;
-}
-
-bool looping = true;
-void stop(__attribute__((unused)) int sig) {
-  printf("Stopping loop...\n");
-  looping = false;
+  return ((now - before) > (WHILE_SLEEP_TIME * 3)) && before != 0;
 }
 
 int main() {
   float dt;
-  time_t last_wakeup;
-  time_t last_sleep;
-  time_t before, now;
+  time_t last_wakeup = load(AWAKE_FILE);
+  time_t last_sleep = load(SLEEP_FILE);
+  time_t before = 0, now;
   // dpms
-  Display *dpy;
-  CARD16 power_level, prev_power_level;
+  Display *display;
+  CARD16 power_level = DPMSModeOn, prev_power_level = DPMSModeOn;
   BOOL dpms_state;
   // upower
-  UpClient *up_client;
-  gboolean on_battery, prev_on_battery;
+  UpClient *up_client = up_client_new();
+  gboolean on_battery = 0, prev_on_battery = 0;
 
-  last_wakeup = load(AWAKE_FILE);
-  last_sleep = load(SLEEP_FILE);
-  before = 0;
-
-  power_level = prev_power_level = DPMSModeOn;
-  if (!(dpy = XOpenDisplay(0)))
+  display = XOpenDisplay(0);
+  if (!display)
     errx(EXIT_FAILURE, "cannot open display '%s'", XDisplayName(0));
-
-  up_client = up_client_new();
-  on_battery = prev_on_battery = 0;
 
   // Unbuffer stdout
   if (setvbuf(stdout, NULL, _IONBF, 0) != 0)
@@ -68,18 +60,15 @@ int main() {
 
   umask(0);
   chdir("/");
-
   signal(SIGTERM, stop);
-
   printf("Starting loop...\n");
 
   while (looping) {
-    sleep(SLEEP_TIME);
+    sleep(WHILE_SLEEP_TIME);
 
     now = time(NULL);
-
     on_battery = up_client_get_on_battery(up_client);
-    DPMSInfo(dpy, &power_level, &dpms_state);
+    DPMSInfo(display, &power_level, &dpms_state);
 
     if (prev_on_battery != on_battery)
       printf("Battery status changed to `%d`\n", on_battery);
@@ -115,6 +104,6 @@ int main() {
     prev_power_level = power_level;
     before = now;
   }
-  XCloseDisplay(dpy);
+  XCloseDisplay(display);
   return EXIT_SUCCESS;
 }
