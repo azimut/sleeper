276adbc20c74e34d0ffe9dfb6347c498a5275150
azimut <azimut.github@protonmail.com>
2022-05-17 03:16:27 -0300

    minor

--- config.h
+++ config.h
@@ -1,9 +1,11 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define DT_DPMS 5.0f
+#define WHILE_SLEEP_TIME 60
+
+#define DT_DPMS       5.0f
 #define DT_SUSPENSION 5.0f
-#define WHILE_SLEEP_TIME 120
+
 #define SLEEP_FILE "sleeptime"
 #define AWAKE_FILE "awaketime"
 
