deb89ce2a8ddf632273b3d69da3e111fd4cc3a86
azimut <azimut.github@protonmail.com>
2022-05-18 05:17:17 -0300

    extracted functions for status changes on loop

--- main.c
+++ main.c
@@ -25,6 +25,24 @@ void stop(__attribute__((unused)) int sig) {
 
 float dt_hours(time_t a, time_t b) { return (a - b) / 60.0f / 60.0f; }
 
+bool suspension_wake(time_t now, time_t before) {
+  return ((now - before) > (WHILE_SLEEP_TIME * 3)) && before != 0;
+}
+
+void check_suspension(time_t now, time_t before, time_t *last_sleep,
+                      time_t *last_wakeup) {
+  if (suspension_wake(now, before)) {
+    *last_sleep = before;
+    *last_wakeup = now;
+    float dt = dt_hours(*last_wakeup, *last_sleep);
+    printf("Suspension sytem wake after `%.2f` hours\n", dt);
+    if (dt > DT_SUSPENSION) {
+      save(AWAKE_FILE, *last_wakeup);
+      save(SLEEP_FILE, *last_sleep);
+    }
+  }
+}
+
 bool dpms_wake(CARD16 prev_pw, CARD16 pw) {
   return (pw == DPMSModeOn) && (prev_pw != pw);
 }
@@ -33,12 +51,30 @@ bool dpms_sleep(CARD16 prev_pw, CARD16 pw) {
   return (pw == DPMSModeOff) && (prev_pw != pw);
 }
 
-bool suspension_wake(time_t now, time_t before) {
-  return ((now - before) > (WHILE_SLEEP_TIME * 3)) && before != 0;
+void check_dpms(time_t now, CARD16 mode, CARD16 prev_mode, time_t *last_sleep,
+                time_t *last_wakeup) {
+  if (dpms_wake(prev_mode, mode)) {
+    *last_wakeup = now;
+    float dt = dt_hours(*last_wakeup, *last_sleep);
+    printf("DPMS screen wake up after `%.2f` hours of sleep\n", dt);
+    if (dt > DT_DPMS) {
+      save(SLEEP_FILE, *last_sleep);
+      save(AWAKE_FILE, *last_wakeup);
+    }
+  }
+  if (dpms_sleep(prev_mode, mode)) {
+    *last_sleep = now;
+    printf("DPMS screen sleeping after `%.2f` hours awake\n",
+           dt_hours(*last_sleep, *last_wakeup));
+  }
+}
+
+void check_upower(gboolean battery, gboolean prev_battery) {
+  if (battery != prev_battery)
+    printf("Battery status changed to `%d`\n", battery);
 }
 
 int main() {
-  float dt;
   time_t last_wakeup = load(AWAKE_FILE);
   time_t last_sleep = load(SLEEP_FILE);
   time_t before = 0, now = time(NULL);
@@ -73,35 +109,9 @@ int main() {
     upower_battery = up_client_get_on_battery(upower_client);
     DPMSInfo(dpms_display, &dpms_mode, &dpms_state);
 
-    if (upower_prev_battery != upower_battery)
-      printf("Battery status changed to `%d`\n", upower_battery);
-
-    if (suspension_wake(now, before)) {
-      last_sleep = before;
-      last_wakeup = now;
-      dt = dt_hours(last_wakeup, last_sleep);
-      printf("Suspension sytem wake after `%.2f` hours\n", dt);
-      if (dt > DT_SUSPENSION) {
-        save(AWAKE_FILE, last_wakeup);
-        save(SLEEP_FILE, last_sleep);
-      }
-    }
-
-    if (dpms_wake(dpms_prev_mode, dpms_mode)) {
-      last_wakeup = now;
-      dt = dt_hours(last_wakeup, last_sleep);
-      printf("DPMS screen wake up after `%.2f` hours of sleep\n", dt);
-      if (dt > DT_DPMS) {
-        save(SLEEP_FILE, last_sleep);
-        save(AWAKE_FILE, last_wakeup);
-      }
-    }
-
-    if (dpms_sleep(dpms_prev_mode, dpms_mode)) {
-      last_sleep = now;
-      printf("DPMS screen sleeping after `%.2f` hours awake\n",
-             dt_hours(last_sleep, last_wakeup));
-    }
+    check_upower(upower_battery, upower_prev_battery);
+    check_suspension(now, before, &last_sleep, &last_wakeup);
+    check_dpms(now, dpms_mode, dpms_prev_mode, &last_sleep, &last_wakeup);
   }
   XCloseDisplay(dpms_display);
   return EXIT_SUCCESS;
