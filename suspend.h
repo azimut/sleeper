e8c4ad58cd870a5c2d265409023d2ae1e04faa10
azimut <azimut.github@protonmail.com>
2022-05-18 12:42:23 -0300

    fix missing ;

--- suspend.h
+++ suspend.h
@@ -4,6 +4,6 @@
 #include <time.h>
 
 void suspension_check(time_t now, time_t before, time_t *last_sleep,
-                      time_t *last_wakeup)
+                      time_t *last_wakeup);
 
 #endif /* SUSPEND_H */
