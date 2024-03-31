a1d1890291c174d272f3858d203078e7fdf3c9eb
azimut <azimut.github@protonmail.com>
2024-03-31 05:19:32 -0300

    fix? sleep time unit
    no clue, why it broke...this is still using file based timestamp

--- src/config.h
+++ src/config.h
@@ -5,8 +5,8 @@
 
 #define WHILE_SLEEP_TIME 60
 
-#define DT_DPMS 5.0f
-#define DT_SUSPENSION 5.0f
+#define DT_DPMS 5.0f * 60 * 60
+#define DT_SUSPENSION 5.0f * 60 * 60
 
 #define SLEEP_FILE "sleeptime"
 #define AWAKE_FILE "awaketime"
