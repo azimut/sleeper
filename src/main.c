77d0a0a28aa5e8113045f49e0add8d0bb671d1ff
azimut <azimut.github@protonmail.com>
2024-04-11 13:58:12 -0300

    solves #5 replaced sysinfo with utmp, also adds sleep time

--- src/main.c
+++ src/main.c
@@ -7,13 +7,14 @@
 #include "./utils.h"
 
 #include <err.h>
+#include <paths.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <sys/sysinfo.h>
 #include <time.h>
 #include <unistd.h>
+#include <utmp.h>
 
 bool quit = false;
 
@@ -30,17 +31,24 @@ int main(void) {
   time_t last_wakeup = load(AWAKE_FILE);
   printf("Last wakeup: %s\n", format_date(last_wakeup));
 
-  struct sysinfo s_info;
-  if (sysinfo(&s_info) != 0)
-    return EXIT_FAILURE;
+  time_t last_reboot, last_shutdown;
+  struct utmp data;
+  FILE *fp = fopen(_PATH_WTMP, "r");
+  memset(&data, 0, sizeof(struct utmp));
+  while (fread(&data, sizeof(struct utmp), 1, fp) == 1) {
+    if (strcmp("shutdown", data.ut_user) == 0)
+      last_shutdown = data.ut_tv.tv_sec;
+    if (strcmp("reboot", data.ut_user) == 0)
+      last_reboot = data.ut_tv.tv_sec;
+  }
 
-  time_t last_poweron = time(NULL) - s_info.uptime;
-  printf("System uptime: %d\n", (int)s_info.uptime);
-  printf("System up since: %s\n", format_date(last_poweron));
+  printf("System up since: %s\n", format_date(last_reboot));
   if ((time(NULL) - last_wakeup) / 60 / 60 > DT_SHUTDOWN) {
-    last_wakeup = last_poweron;
-    save(AWAKE_FILE, last_poweron);
-    printf("Fast-forwarding wakeup time to match uptime.\n");
+    last_wakeup = last_reboot;
+    last_sleep = last_shutdown;
+    save(SLEEP_FILE, last_sleep);
+    save(AWAKE_FILE, last_wakeup);
+    printf("Fast-forwarding wakeup/sleep time to match uptime.\n");
   }
 
   time_t before = 0;
